85f1f5f705a41bbeadb5317314b7fdfc1610c07b
manato-alt <cococara988@gmail.com>
2023-04-15 21:15:21 +0900

    日本語翻訳を追加

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,9 @@ module Portfolio
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.i18n.default_locale = :ja
+    config.time_zone = "Tokyo" 
+    config.active_record.default_timezone = :local
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.yml').to_s]
   end
 end
