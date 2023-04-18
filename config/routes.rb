37160c2a75af86be01d899374f19b24cac8308f4
manato-alt <cococara988@gmail.com>
2023-04-18 10:30:36 +0900

    ゲーム選択ページへのrootを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'games/index'
   root 'tops#index'
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
   resources :users, only: %i[new create]
+  resources :games, only: %i[index]
 end
