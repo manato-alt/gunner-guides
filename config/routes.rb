f2634770a83795febe9845f25f4019341162141d
manato-alt <cococara988@gmail.com>
2023-05-06 15:05:47 +0900

    目的選択ページ及び遷移先のルートを追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
   resources :users, only: %i[new create]
-  resources :games, only: %i[index]
+  resources :games, only: %i[index show], param: :title do
+    member do
+      get 'setting', to: 'purposes#setting'
+      get 'training', to: 'purposes#training'
+      get 'information', to: 'purposes#information'
+    end
+  end
 end
