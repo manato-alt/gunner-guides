d5ea8599ab236535faebb7ca52cda0309838d8c3
manato-alt <cococara988@gmail.com>
2023-05-17 10:19:58 +0900

    詳細ページ以降後視聴済みと判断する処理を追加

--- app/controllers/purposes_controller.rb
+++ app/controllers/purposes_controller.rb
@@ -18,5 +18,11 @@ class PurposesController < ApplicationController
   def show
     @game = Game.find_by!(title: params[:title])
     @video = Video.find(params[:id])
+    if current_user
+      @uservideo = Uservideo.find_by(user_id: current_user.id, video_id: @video.id)
+      unless @uservideo
+        @uservideo = Uservideo.create(user_id: current_user.id, video_id: @video.id, watched_status: 1, watched_at: Time.now)
+      end
+    end
   end
 end
