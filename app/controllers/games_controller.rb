ca08b18b599e99d5060cb2c4921abad2775b1f7b
manato-alt <cococara988@gmail.com>
2023-06-13 01:41:51 +0900

    ページネーションを追加、検索フォームから送られた値を曖昧検索処理を追加

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,10 @@
 class GamesController < ApplicationController
   def index
-    @games = Game.all
+    @games = Game.page(params[:page]).per(12)
+    
+    if params[:title].present?
+      @games = @games.where("title LIKE ?", "%#{params[:title]}%")
+    end
   end
 
   def show
