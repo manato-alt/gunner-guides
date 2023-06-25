46ca88cc41a00d8d0759c85a38e152e03f8b6914
manato-alt <cococara988@gmail.com>
2023-06-25 23:19:38 +0900

    関連するオブジェクトがデータベースから削除される処理を追加

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,7 @@
 class Video < ApplicationRecord
   belongs_to :category
   belongs_to :game
-  has_many :user_videos
+  has_many :user_videos, class_name: 'Uservideo', dependent: :destroy
   has_many :users, through: :user_videos
+  has_many :memos, dependent: :destroy
 end
