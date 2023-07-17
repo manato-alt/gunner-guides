27b459db5a779d49f71bb50ae917e526805ce50e
manato-alt <cococara988@gmail.com>
2023-07-17 17:04:02 +0900

    validateを追加

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,4 +1,5 @@
 class Playlist < ApplicationRecord
+  validates :title, presence: true
   belongs_to :user
   has_many :playlist_videos, dependent: :destroy
   has_many :videos, through: :playlist_videos
