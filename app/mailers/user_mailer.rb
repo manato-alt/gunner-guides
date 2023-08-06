01a383297cdfc8c92c705997966fcded2b0668b0
manato-alt <cococara988@gmail.com>
2023-08-06 11:19:52 +0900

    UserMailer という名前でパスワードリセット用のメイラーを作成

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,16 @@
+class UserMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.reset_password_email.subject
+  #
+  def reset_password_email(user)
+    @user = User.find(user.id)
+    #このurlでリダイレクトするとパスワードがリセットされる。
+    @url = edit_password_reset_url(@user.reset_password_token)
+    #toは宛先、subjectは件名を指定する。
+    mail(:to => user.email,
+        :subject => "パスワードリセット")
+  end
+end
